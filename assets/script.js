fd2f31ab1abd00833cc168584fc2245e43bac530
williamhillarnold@yahoo.com <97689293+will9105@users.noreply.github.com>
2022-06-21 22:52:21 -0400

    questions added

---
+++ assets/script.js
@@ -0,0 +1,63 @@
+
+let questionsArr = [
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        choice_A: '<js>',
+        choice_B: '< scripting >',
+        choice_C: '<script>',
+        choice_D: '<javascript>',
+        answer: 'C',
+    },
+
+    {
+        question: 'Why so JavaScript and Java have similar name?',
+        choice_A: 'JavaScript is a stripped-down version of Java',
+        choice_B: 'JavaScripts syntax is loosely based on Java',
+        choice_C: 'They both originated on the island of Java',
+        choice_D: 'None of the above',
+        answer: 'B',
+    },
+            {
+        question: 'What are variables used for in JavaScript Programs?',
+        choice_A: 'Storing numbers, dates, or other values',
+        choice_B: 'Varying randomly',
+        choice_C: 'Causing high- school algebra flashbacks',
+        choice_D: 'None of the above',
+        answer: 'A',
+    },
+
+    {
+        question_A: 'Which of the following is not considered a JavaScript operator?',
+        choice_A: 'new',   
+        choice_B: 'this',
+        choice_C: 'delete',
+        choice_D: 'typeof',
+        answer: 'B',
+    },
+
+    {
+        question: 'Using _______ statement is how you test for a specific condition.',
+        choice_A: 'Select', 
+        choice_B: 'If', 
+        choice_C: 'Switch', 
+        choice_D: 'For',
+        answer: 'B'
+    },
+
+    {
+        question: 'What is mean by "this" keyword in javascript?',
+        choice_A: 'It refers current object', 
+        choice_B: 'It referes previous object', 
+        choice_C: 'It is variable which contains value',
+        choice_D: 'None of the above',
+        answer: 'A'
+    },
+
+    {
+        question: 'In JavaScript, Window.prompt() method return true or false value ?',
+        choice_A: 'False', 
+        choice_B:  'True',
+        Answer: 'A'
+    }
+]
+
